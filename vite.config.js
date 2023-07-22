4594e8f8d1ee55979a1b40dc23f99b0b8632ac5b
Jaay06 <josekoyi9@gmail.com>
2023-07-22 20:16:42 +0100

    adds drill

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import path from 'path'
+import { defineConfig } from "vite"
+import { viteStaticCopy } from "vite-plugin-static-copy"
+import path from "path"
 
 export default defineConfig({
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: path.resolve(__dirname, './assets') + '/[!.]*', // 1️⃣
-                    dest: './assets', // 2️⃣
-                },
-            ],
-        }),
-    ]
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, "./assets") + "/[!.]*", // 1️⃣
+          dest: "./assets", // 2️⃣
+        },
+      ],
+    }),
+  ],
 })
